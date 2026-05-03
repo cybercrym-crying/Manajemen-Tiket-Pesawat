367f5ac92c4856643f6985495e92b8c2de2213cf
RYDZHR <riyadhzuhair71@gmail.com>
2026-05-03 22:56:22 +0800

    Nyelesain airline dan sedikit nambahin di types.h

--- src/airline.cpp
+++ src/airline.cpp
@@ -4,26 +4,62 @@
 #include "../header/types.h"
 #include <fstream>
 #include <iostream>
+#include <sstream>
 #include <string>
 #include <vector>
 
 // using namespace std;
 
+
 // tampilan menu airline pilihan aksi atau keluar aksi
 void airlineMenu(const string username, vector<User> &user,
                  vector<Flight> &flights, vector<Ticket> &ticket) {
-  std::cout << "1. Transaksi" << std::endl;
-  std::cout << "2. Penerbangan Baru" << std::endl;
-  std::cout << "3. delete" << std::endl;
+  std::cout << "1. Transaction" << std::endl;
+  std::cout << "2. New Flight Schedule" << std::endl;
+  std::cout << "3. Remove Flight" << std::endl;
   std::cout << "4. View Flight" << std::endl;
 }
+
+std::vector<Ticket> ticketlist;
 
 // ambil data dari tiket.csv dan filter lalu cari sesuai flightsID
 // cek apakah airlane name sesuai dengan username
 void viewTotalTransaction(const string username) {
   fstream fin;
 
-  fin.open("data/ticket.csv", ios::in);
+  fin.open("data/ticket.csv", ios::in); //ambil data tiket
+
+  string line;
+  getline(fin, line);
+
+  while(std::getline(fin, line)){
+    std::stringstream ss(line);
+    std::string token;
+    Ticket t;
+
+    std::getline(ss, token, ','); 
+    t.flightID = stoi(token);
+    std::getline(ss, t.customerName, ',');
+    std::getline(ss, t.flightID, ',');
+    std::getline(ss, t.seatNumber, ',');
+    std::getline(ss, t.date);
+
+    ticketlist.push_back(t);
+  }
+
+  for (auto& t : ticketlist) {
+    if (t.customerName == username){
+      std::cout << "Flight ID : " << t.flightID << "\n";
+      std::cout << "Name      : " << t.customerName << "\n";
+      std::cout << "SeatNumber: " << t.seatNumber << "\n";
+      std::cout << "Date : " << t.date << "\n";
+
+    }
+  }
+
+  fin.close();
+
+  
 }
 
 void createFlight(const string username, vector<Flight> &flights) {
